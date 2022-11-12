478d3689fbd320a4f2d3d6782bab7114c7db1007
lucasrenandns <lucasrenandns96@gmail.com>
2022-11-12 16:14:00 -0300

    Update pokedex

--- assets/js/pokedex.js
+++ assets/js/pokedex.js
@@ -1,3 +1,4 @@
+// ATRIBUINDO OS ELEMENTOS EM SUAS VARIÁVEIS
 const pokemonNumber = document.querySelector(".pokemon_number")
 const pokemonName = document.querySelector(".pokemon_name")
 const pokemonImage = document.querySelector(".pokemon_image")
@@ -6,8 +7,10 @@ const search = document.querySelector(".search")
 const buttonPrev = document.querySelector(".btn_prev")
 const buttonNext = document.querySelector(".btn_next")
 
+// VARIÁVEL PARA RENDERIZAR IMAGEM
 let renderImage = 1
 
+// FUNÇÃO PARA BUSCAR POKEMON + DADOS (VIA API)
 const fetchPokemon = async (pokemon) => {
     const responseAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
@@ -17,6 +20,7 @@ const fetchPokemon = async (pokemon) => {
     }
 }
 
+// FUNÇÃO PARA RENDERIZAR POKEMON + DADOS
 const renderPokemon = async (pokemon) => {
     pokemonName.textContent = "Loading..."
     pokemonNumber.textContent = ""
@@ -36,21 +40,24 @@ const renderPokemon = async (pokemon) => {
         pokemonNumber.textContent = ""
     }
 }
+// CHAMANDO A FUNÇÃO RENDERIZAR POKEMON NO DOCUMENTO
 renderPokemon(renderImage)
 
+// ADICIONANDO EVENTO NO FORMULÁRIO
 form.addEventListener("submit", (event) => {
      event.preventDefault()
      renderPokemon(search.value.toLowerCase())
 })
 
+// FUNÇÃO PARA A IMAGEM ANTERIOR
 buttonPrev.addEventListener("click", () => {
-
     if(renderImage > 1) {
         renderImage = renderImage - 1
         renderPokemon(renderImage)
     }
 })
 
+// FUNÇÃO PARA A PRÓXIMA IMAGEM
 buttonNext.addEventListener("click", () => {
     renderImage = renderImage + 1
     renderPokemon(renderImage)
